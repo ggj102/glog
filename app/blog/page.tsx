a012a2fb5c117dd011e50b62817e23044bd4e646
kjkim <kjkim@xsinc.co.kr>
2024-02-02 17:21:32 +0900

    보류

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,30 +1,36 @@
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import { genPageMetadata } from 'app/seo'
+// import ListLayout from '@/layouts/ListLayoutWithTags'
+// import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+// import { allDevlogs, allPortfolios, allRetrospects, allSkills } from 'contentlayer/generated'
+// import { genPageMetadata } from 'app/seo'
 
-const POSTS_PER_PAGE = 5
+// const POSTS_PER_PAGE = 5
 
-export const metadata = genPageMetadata({ title: 'Blog' })
+// export const metadata = genPageMetadata({ title: 'Blog' })
 
-export default function BlogPage() {
-  const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
-  const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
-  )
-  const pagination = {
-    currentPage: pageNumber,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
+// export default function BlogPage() {
+//   const arr = [allDevlogs, allPortfolios, allRetrospects, allSkills]
 
-  return (
-    <ListLayout
-      posts={posts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      title="All Posts"
-    />
-  )
-}
+//   const reduce = arr.reduce((acc, val) => {
+//     return acc.concat(val)
+//   }, [])
+
+//   const posts = allCoreContent(sortPosts(reduce))
+//   const pageNumber = 1
+//   const initialDisplayPosts = posts.slice(
+//     POSTS_PER_PAGE * (pageNumber - 1),
+//     POSTS_PER_PAGE * pageNumber
+//   )
+//   const pagination = {
+//     currentPage: pageNumber,
+//     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+//   }
+
+//   return (
+//     <ListLayout
+//       posts={posts}
+//       initialDisplayPosts={initialDisplayPosts}
+//       pagination={pagination}
+//       title="All Posts"
+//     />
+//   )
+// }
